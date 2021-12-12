7c3a549d8b262a80f0666aa56c92975bb55d75f0
Fede Conte Grand <fede.contegrand@gmail.com>
2021-12-12 19:06:47 -0300

    stilos footer

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -28,7 +28,6 @@ const CharactersProvider = ({ children }) => {
             let isPar=page%2===0
             let half
             isPar ? half=data.results.slice(10,data.results.length) : half= data.results.slice(0,data.results[9] ? 10 : data.results.length)
-            console.log(half)
             setCharacters(half)
           }
           else setCharacters({error:data.error})
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer({page,handlePageChange,limitPage}) {
     return (
         <div style={{display:'flex',width:'100%',justifyContent:'center', columnGap:'1rem',alignItems:'center',padding:'1rem 0'}}>
             <button onClick={handlePageChange} name='prev' className={styles.footerButtons} disabled={page===1}>Prev</button>
-            <span>{page}</span>
+            <span className={styles.page}>{page}</span>
             <button onClick={handlePageChange} name='next' className={styles.footerButtons} disabled={page===limitPage}>Next</button>
         </div>
     )
